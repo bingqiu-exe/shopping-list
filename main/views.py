40215c6733b846e90493442a3d4be2a1f2746282
bingqiu-exe <nasywakamila2015@gmail.com>
2023-09-13 10:48:39 +0700

    <pesan_commit>

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Nasywa Kamila Az Zahra',
-        'class': 'PBP B'
+        'Name of Sponsor',
+        'Kim Dokja'
     }
 
     return render(request, "main.html", context)
